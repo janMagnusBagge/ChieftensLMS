57c1422539d820368cd4238d8899ad4bbe518452
sinth <sinth@DESKTOP-T3MVRNP>
2016-08-15 10:36:55 +0200

    Course list all as angular

---
+++ ChieftensLMS/Controllers/Api/CourseApiController.cs
@@ -0,0 +1,62 @@
+﻿using ChieftensLMS.DAL;
+using ChieftensLMS.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChieftensLMS.Controllers.Api
+{
+	public class CourseApiController : Controller
+	{
+		private LMSDbContext _context;
+		private CourseService _courseService;
+
+		public CourseApiController()
+		{
+			_context = new LMSDbContext();
+			_courseService = new CourseService(_context);
+		}
+
+		// GET: Courses
+		public ActionResult Index()
+		{
+			var courses = _courseService.GetCoursesForUserId(User.Identity.GetUserId())
+				.Select(c => new
+					{
+						c.Id,
+						c.Name,
+						c.Description
+					}
+				);
+
+			var returnObject = new
+			{
+				data = new { courses = courses },
+				success = true
+			};
+
+			return Json(returnObject, JsonRequestBehavior.AllowGet);
+		}
+
+		//// GET: Courses/Details/5
+		//public ActionResult Details(int? id)
+		//{
+		//	if (id == null)
+		//	{
+		//		return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+		//	}
+
+		//	var course = _courseService.GetCourseById((int)id);
+
+		//	if (course == null)
+		//	{
+		//		return HttpNotFound();
+		//	}
+		//	return View(course);
+		//}
+
+	}
+}
